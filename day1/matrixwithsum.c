8c6378027083ae385b0b78fcf3d84ecf4f70f18f
Mohd Faraz <55929239+fzmalik@users.noreply.github.com>
2020-05-29 04:50:31 +0530

    Update matrixwithsum.c

--- day1/matrixwithsum.c
+++ day1/matrixwithsum.c
@@ -9,16 +9,8 @@ int main()
 		{
 			for(j=0;j<=2;j++)
 			{
-				scanf("%d",&arr[i][j]);
-			}	
-		}	 
-	printf("\n your entered matrix is : \n");
-	     for(i=0;i<=2;i++)
-		{
-			for(j=0;j<=2;j++)
-			{   
-			
-				printf("%d",arr[i][j]);   
+				scanf("%d",&arr[i][j]); 
+	                        printf("%d",arr[i][j]);   
 				sum+=arr[i][j];
 			}
 			
